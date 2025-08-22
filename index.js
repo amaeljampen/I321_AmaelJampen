e0f7a9c1d40fc1658a57b02c8aabf17a9c743cb1
amaeljampen <amael.jampen@eduvaud.ch>
2025-08-22 09:26:49 +0200

    commentaires express

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ console.log(multiply([1,2,3,4,5]))
 
 // Test express
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express') // import express
+const app = express() // créer l'application et ses routes
+const port = 3000 // port choisi pour l'app
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+}) // lorsque rien n'est ajouté aprés le lien (accueil du site), get envoie les infos du res et req est la requete de l'user
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+}) // écoute le port choisi
